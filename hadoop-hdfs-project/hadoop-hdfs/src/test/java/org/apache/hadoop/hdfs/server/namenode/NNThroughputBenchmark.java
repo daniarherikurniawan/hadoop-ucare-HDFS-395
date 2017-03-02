f00f0ad5d460c8be808119e7cbe7b2aad0f34515
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-03-03 00:51:56 +0700

    update NN codes

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/NNThroughputBenchmark.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/NNThroughputBenchmark.java
@@ -853,10 +853,10 @@ public class NNThroughputBenchmark {
       blocks.add(blk);
       nrBlocks++;
       LOG.info("nrBlocks = " + nrBlocks);
-      if(nrBlocks % 3000 == 0){
+      if(nrBlocks % 1000 == 0){
         try 
         {
-            Thread.currentThread().sleep(30);
+            Thread.currentThread().sleep(1000);
         } 
         catch(InterruptedException e)
         {
